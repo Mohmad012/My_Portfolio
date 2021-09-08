9506a2a1aee696ed208dd42a2523e90f44eb913c
mohmad <mohmadailsia@gmail.com>
2021-09-08 22:13:57 +0200

    update my info

--- src/components/About/myInfo.js
+++ src/components/About/myInfo.js
@@ -1,4 +1,12 @@
-let myInfo =`	I'm a Creative Frontend Developer with <strong> 3+ years </strong> hands of experience in building <strong>responsive websites. </strong>
+let myInfo =`	I'am <strong> a Full Stack Developer </strong> with <strong> experience in Building UI components </strong> using react,
+				managing application state using Redux, Rendering App on server using next.js,
+				handling client-side routing using React-router-dom and Node.js,
+				Express.js As a Backend and MongoDB, PostgreSQL As a Database using Git As version control.
+				Passionate about clean code and get things done.
+			`
+
+let myInfo1 = `
+				I'm a Creative Frontend Developer with <strong> 3+ years </strong> hands of experience in building <strong>responsive websites. </strong>
 				contributing to all phases of <strong> SDLC </strong> from conception to maintenance by acting as analyst, architect to
 				<strong> finished of 10+ projects </strong> and you can check links for it in projects section at the bottom. Cross-
 				functional <strong> Scrum agile member. </strong> Passionate about <strong> clean code </strong> and <strong> get things done. </strong> Maintains up to
